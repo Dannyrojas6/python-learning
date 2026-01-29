09cc64edc200702f11938ef4e5e4435ae4b4b920
Dannyrojas6 <2869217456@qq.com>
2026-01-29 13:48:06 +0800

    python to json

--- src/main.py
+++ src/main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from python-learning!")
+import json
+from pathlib import Path
 
-
-if __name__ == "__main__":
-    main()
+# 将python数据对象转换为json
+data = {
+    "tasks": [
+        {"id": 1, "content": "buy milk", "done": False, "created_data": "2026-1-1"},
+        {
+            "id": 2,
+            "content": "go to supermarket",
+            "done": True,
+            "created_data": "2026-1-3",
+        },
+    ]
+}
+with open("task.json", "w", encoding="utf-8") as f:
+    json.dump(data, f)
