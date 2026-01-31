1b9b402687b2077b1aed097641822dab9265e561
Dannyrojas6 <2869217456@qq.com>
2026-01-31 13:41:17 +0800

    1.0正式发布！

--- src/main.py
+++ src/main.py
@@ -1,64 +1,131 @@
 import json
 from pathlib import Path
 
-# 将python数据对象转换为json
-# data = {
-#     "tasks": [
-#         {"id": 1, "content": "buy milk", "done": False, "created_date": "2026-1-1"},
-#         {
-#             "id": 2,
-#             "content": "go to supermarket",
-#             "done": True,
-#             "created_date": "2026-1-3",
-#         },
-#     ]
-# }
 
-# data_format = {"id": id, "content": content, "done": done, "created_date": created_date}
-#
-# with open("src/task.json", "a", encoding="utf-8") as f:
-#     json.dump(data_format, f)
+def add_func():
+    data: dict = {"tasks": []}
+    if Path("src/task.json").exists():
+        id = int(input("Task id: "))
+        content = input("content: ")
+        done = input("done: ").strip().lower() == "true"
+        created_date = str(input("created_date: "))
 
-data: dict = {"tasks": []}
-if Path("src/task.json").exists():
-    id = int(input("Task id: "))
-    content = input("content: ")
-    done = input("done: ").strip().lower() == "true"
-    created_date = str(input("created_date: "))
+        with open("src/task.json") as f:
+            try:
+                data = json.load(f)
+            except Exception as e:
+                print(f"文件读取错误,原因：{e}")
 
-    with open("src/task.json") as f:
-        try:
-            data = json.load(f)
-        except Exception as e:
-            print(f"文件读取错误,原因：{e}")
+            new_task = {
+                "id": id,
+                "content": content,
+                "done": done,
+                "created_date": created_date,
+            }
+            data["tasks"].append(new_task)
+            with open("src/task.json", "w", encoding="utf-8") as f:
+                json.dump(data, f, indent=4, ensure_ascii=False)
 
-        new_task = {
-            "id": id,
-            "content": content,
-            "done": done,
-            "created_date": created_date,
+    else:
+        data_origin = {
+            "tasks": [
+                {
+                    "id": 1,
+                    "content": "buy milk",
+                    "done": False,
+                    "created_date": "2000-01-01",
+                },
+                {
+                    "id": 2,
+                    "content": "play minecraft",
+                    "done": False,
+                    "created_date": "2001-01-01",
+                },
+                {
+                    "id": 3,
+                    "content": "listen to music",
+                    "done": False,
+                    "created_date": "1999-09-09",
+                },
+            ]
         }
-        data["tasks"].append(new_task)
-        with open("src/task.json", "w", encoding="utf-8") as f:
-            json.dump(data, f, indent=4, ensure_ascii=False)
+        with open("src/task.json", "w") as f:
+            json.dump(data_origin, f, indent=4, ensure_ascii=False)
+            print("初始化成功！请再次运行脚本。")
 
-else:
-    data_origin = {
-        "tasks": [
-            {
-                "id": 1,
-                "content": "buy milk",
-                "done": False,
-                "created_date": "2000-01-01",
-            },
-            {
-                "id": 2,
-                "content": "play minecraft",
-                "done": True,
-                "created_date": "2001-01-01",
-            },
-        ]
-    }
-    with open("src/task.json", "w") as f:
-        json.dump(data_origin, f, indent=4, ensure_ascii=False)
-        print("初始化成功！请再次运行脚本。")
+
+def list_func():
+    with open("src/task.json", encoding="utf-8") as f:
+        data = json.load(f)
+        for i in data["tasks"]:
+            tem_list = []
+            for j in i.values():
+                tem_list.append(j)
+            print(
+                f"id:{tem_list[0]},content:{tem_list[1]},done:{tem_list[2]},created_date:{tem_list[3]}"
+            )
+
+
+def display_func():
+    with open("src/task.json", encoding="utf-8") as f:
+        data = json.load(f)
+        print(data)
+
+
+def done_func(arg):
+    with open("src/task.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+        # for value in enumerate(data["tasks"][arg]):
+        #     if value["done"][arg]=="False":
+        #         data["tasks"][arg]=="True"
+        # data_dict = data["tasks"]
+        try:
+            data["tasks"][arg]["done"] = True
+        except Exception as e:
+            print(f"Error!reason:({e})")
+    with open("src/task.json", "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
+
+
+def remove_func(arg):
+    with open("src/task.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+        # for key, value in enumerate(data_dit):
+        #     if value["id"] == 1:
+        #         data_dit.pop(key)
+        #         break
+        # print(data_dit)
+        data["tasks"] = [task for task in data["tasks"] if task["id"] != arg]
+        # print(data)
+    with open("src/task.json", "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
+
+
+def main():
+    while True:
+        print("\n")
+        print("1.添加任务")
+        print("2.删除任务")
+        print("3.列出所有任务")
+        print("4.标记任务完成")
+        print("0.退出")
+        arg = int(input("请输入功能编号："))
+        match arg:
+            case 1:
+                add_func()
+            case 2:
+                remove_index = int(input("请输入想要删除的任务编号："))
+                remove_func(remove_index)
+            case 3:
+                list_func()
+            case 4:
+                done_index = int(input("请输入标记完成任务的编号："))
+                done_func(done_index)
+            case 0:
+                break
+            case _:
+                print("无效输入！请重试。")
+
+
+if __name__ == "__main__":
+    main()
